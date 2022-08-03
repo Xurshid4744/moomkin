d6d8d8b411a4c9c02fe5f46c93a4e2fb1c67ad6f
Alisher <umarovalisher132@gmail.com>
2022-08-03 16:39:52 +0500

    added new page

---
+++ src/pages/Services/ITStaff/helper.js
@@ -0,0 +1,45 @@
+import img17 from "../../../assets/icons/Research.svg";
+import img18 from "../../../assets/icons/Research1.svg";
+import img19 from "../../../assets/icons/Analysis2.svg";
+import img20 from "../../../assets/icons/Analysis1.svg";
+import img21 from "../../../assets/icons/Design.svg";
+import img22 from "../../../assets/icons/Design1.svg";
+import img23 from "../../../assets/icons/Implementation.svg";
+import img24 from "../../../assets/icons/Implementation1.svg";
+
+
+export const animationCardLeft = [
+    {
+        img1: img17,   
+        img2: img18,
+        title: "Step 1: Requirements gathering",
+        ul: [
+            { li: "Describe to us a set of skills you need or a task to be done. Maybe you have a role description so we can pick the right remote worker. If you do not, we identify what a specialist you need. " },
+        ],
+    },
+    {
+        img1: img21,
+        img2: img22,
+        title: "Step 3: Onboarding a remote developer",
+        ul: [
+            { li: "Plunge developers into the working environment, introduce them to your in-house team, give access to project management tools you use, let them become familiar with a project concept. Extended team members adjust to your internal schedule and fully integrate into your in-house team, even though they stay remote." },
+        ],
+    },
+];
+
+export const animationCardRight = [
+    {
+        img1: img20,
+        img2: img19,
+        title: "Step 2: Look through a list of suitable candidates",
+        ul: [
+            { li: "Taking into account your requirements, we provide a suitable candidate or a list of them in the case when you want to carry out the final interview by yourself. When you accept our candidate, after settling terms and conditions, signing contracts and NDAs, the specialist is ready to become a part of your in-house team." },
+        ],
+    },
+    {
+        img1: img23,
+        img2: img24,
+        title: "Step 4: Get the software done!",
+        ul: [{ li: "Whether you needed a developer with a certain specific technical skillset or you wanted to extend the in-house team, now you've empowered your team with SumatoSoft's specialists and can be sure that your project will be developed on time and in the right way." }]
+    },
+];
